92b0baea35f868d30a4c1f17b0079022ef6e1bc1
Fatimatou Bah <faatimabah@gmail.com>
2026-09-19 15:30:16 +0100

    Create Express server and test route

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+//Middleware - Starting the server
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+//Test route
+app.get("/", (req, res) => {
+    res.status(200).send("Welcome to Saving Tracker API");
+})
+
+//Start the server
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
