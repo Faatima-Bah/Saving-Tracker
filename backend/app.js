c9950e36a111a6ac069893942a977f333c11c581
Fatimatou Bah <faatimabah@gmail.com>
2026-09-22 22:15:32 +0100

    Test database connection

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,24 @@ app.use(cors());
 //Test route
 app.get("/", (req, res) => {
     res.status(200).send("Welcome to Saving Tracker API");
-})
+});
+
+//Test database connection
+app.get ("/db-test", async(req, res) => {
+    try {
+        const [tables] = await db.query("SHOW TABLES")
+        res.status(200).json({
+            message: "Database connected successfully",
+            tables: tables
+        });
+    } catch (error) {
+        console.error("Database connection error:", error);
+
+        res.status(500).json({
+            message: "Database connection failed"
+        });
+    }
+});
 
 //Start the server
 app.listen(PORT, () => {
